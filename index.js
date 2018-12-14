5b2f1023a8b8a438dd8648828f9fcecacb9393d1
Michael Trew <michaeltrew@Michaels-iMac.attlocal.net>
2018-12-13 23:22:16 -0500

    entered endpoints and knex

--- index.js
+++ index.js
@@ -1,27 +1,62 @@
 const express = require('express');
 const server = express();
-
+const knex = require('knex');
+const dbConfig = (require('./knexfile'))
 server.use(express.json());
-const PORT = 5000;
+const port = 5000;
+const db = knex(dbConfig.development);
 
-server.post('/crayons', (req,res) => {
+server.post('/crayons', (req, res) => {
+  const crayon = req.body;
+  db('crayons').insert(crayon)
+    .then(ids => {
+      res.status(201).json(ids)
+    })
+    .catch( err => {res.status(500).json({ err: 'failed to insert crayon' })});
+  // INSERT INTO crayons (color,percent_used) VALUES('red', .9)
+});
+
+server.get('/crayons', (req, res) => {
+  db('crayons')
+    .then(rows => {
+      res.json(rows)
+    })
+    .catch(err => res.status(500).json({ err: "failed to get crayons" }))
 
 });
 
-server.get('/crayons', (req,res) => {
-
+server.get('/crayons/:id', (req, res) => {
+  const { id } = req.params;
+  db('crayons').where('id', id)
+    .then(rows => {
+      res.json(rows)
+    })
+    .catch(err => {
+      res.status(500).json({ err: "error getting your crayon" })
+    })
 });
 
-server.get('/crayons:id', (req,res) => {
-
+server.delete('/crayons/:id', (req, res) => {
+  const { id } = req.params;
+  db('crayons')
+    .where('id', id)
+    .del()
+    .then(count => {
+      res.json(count)
+      .catch(err => {
+        res.status(500).json({err: 'cannot delete that crayon'})
+      })
+    })
 });
 
-server.delete('/crayons:id', (req, res) => {
-
+server.put('/crayons/:id', (req, res) => {
+  const { id } = req.params;
+  const crayon = req.body;
+  db('crayons').where('id', id).update(crayon)
+    .then(rowCount => {
+      res.status(200).json(rowCount)
+    })
+    .catch(err => {res.status(500).json({ err: "trouble updating you're crayon" })})
 });
 
-server.put('/crayons:id',(req,res) => {
-
-});
-
-server.listen(port,console.log(`server is listening on port ${port}`))
+server.listen(port, console.log(`server is listening on port ${port}`))
